64499d40709ec586460ce7a0d2057e15cca244e0
Romain-len <89740581+romain-lnr@users.noreply.github.com>
2023-01-10 23:35:27 +0100

    Modify project

--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -1,127 +1,129 @@
 var httpRequest; //Une variable pour stocker la requête ajax
 
-//Recherche des éléments de la page que nous allons manipuler avec JS
+// Relation avec les boutons de la page. Elles servent à repérées les ID de la page HTML.
 var btnville1 = document.querySelector("#b1");
 var btnville2 = document.querySelector("#b2");
 var btnville3 = document.querySelector("#b3");
 var btnville4 = document.querySelector("#b4");
 var btnville5 = document.querySelector("#b5");
 
-//Ajoute l'événement sur le click du bouton charger
+//Ajoute l'événement sur le click du bouton charger. Ces variables vont permettre de lire les informations liée aux boutons, ceux sont des écouteurs d'evenements. 
 btnville1.addEventListener("click", CommentNY);
-btnville1.addEventListener("click", $("table").animate({left: '250px'}));
 btnville2.addEventListener("click", CommentBerlin);
 btnville3.addEventListener("click", CommentMoscou);
 btnville4.addEventListener("click", CommentShangai);
 btnville5.addEventListener("click", CommentSkopje);
 
 
-//Fonction qui charge un texte en ajax
+//Fonction qui execute des données sur New York. 
 function CommentNY() {
     httpRequest = new XMLHttpRequest();
 
-    $("table").animate({left: '250px'});
+// Si la requette ne fonctionne pas, une alerte pop-up s'affichera sur le site et le programme sort de la fonction.
     if (!httpRequest) {
         alert('Giving up :( Cannot create an XMLHTTP instance');
         return false;
     }
-
-    fetch("SCRIPT/data.json") // Lire le fichier JSON
-        //Parse le fichier JSON
+//Cette fonction permet d'analyser et d'extraires des informations de New York
+    fetch("SCRIPT/NY.json") // Lire le fichier JSON
         .then(response => response.json())
         .then(data => {
-            httpRequest.open((document.querySelector("#title").innerText = data['Ville'][0].Title) +
-                (document.querySelector("#text").innerText = data['Ville'][0].Desc) +
-                (document.querySelector("#météo").innerText = data['Ville'][0].Météo) +
-                (document.querySelector("#population").innerText = data['Ville'][0].Population));
+            httpRequest.open((document.querySelector("#title").innerText = data.Title) +
+                (document.querySelector("#text").innerText = data.Desc) +
+                (document.querySelector("#météo").innerText = data.Météo) +
+                (document.querySelector("#population").innerText = data.Population));
         })
     httpRequest.open(document.querySelector("#image").src = "IMG/NY.jpg");
     httpRequest.send();
 }
 
-//Fonction qui charge un texte en ajax
+//Fonction qui execute des données sur Berlin. 
 function CommentBerlin() {
     httpRequest = new XMLHttpRequest();
 
+// Si la requette ne fonctionne pas, une alerte pop-up s'affichera sur le site et le programme sort de la fonction.
     if (!httpRequest) {
         alert('Giving up :( Cannot create an XMLHTTP instance');
         return false;
     }
-
-    fetch("SCRIPT/data.json") // Lire le fichier JSON
-        //Parse le fichier JSON
+//Cette fonction permet d'analyser et d'extraires des informations de Berlin
+    fetch("SCRIPT/Berlin.json") // Lire le fichier JSON
         .then(response => response.json())
         .then(data => {
-            httpRequest.open((document.querySelector("#title").innerText = data['Ville'][1].Title) +
-                (document.querySelector("#text").innerText = data['Ville'][1].Desc) +
-                (document.querySelector("#météo").innerText = data['Ville'][1].Météo) +
-                (document.querySelector("#population").innerText = data['Ville'][1].Population));
+            httpRequest.open((document.querySelector("#title").innerText = data.Title) +
+                (document.querySelector("#text").innerText = data.Desc) +
+                (document.querySelector("#météo").innerText = data.Météo) +
+                (document.querySelector("#population").innerText = data.Population));
         })
     httpRequest.open(document.querySelector("#image").src = "IMG/Berlin.jpg");
     httpRequest.send();
 }
 
-//Fonction qui charge un texte en ajax
+//Fonction qui execute des données sur Moscou.
 function CommentMoscou() {
     httpRequest = new XMLHttpRequest();
 
+    // Si la requette ne fonctionne pas, une alerte pop-up s'affichera sur le site et le programme sort de la fonction.
     if (!httpRequest) {
         alert('Giving up :( Cannot create an XMLHTTP instance');
         return false;
     }
 
-    fetch("SCRIPT/data.json") // Lire le fichier JSON
-        //Parse le fichier JSON
+    //Cette fonction permet d'analyser et d'extraires des informations de Moscou
+    fetch("SCRIPT/Moscou.json") // Lire le fichier JSON
         .then(response => response.json())
         .then(data => {
-            httpRequest.open((document.querySelector("#title").innerText = data['Ville'][2].Title) +
-                (document.querySelector("#text").innerText = data['Ville'][2].Desc) +
-                (document.querySelector("#météo").innerText = data['Ville'][2].Météo) +
-                (document.querySelector("#population").innerText = data['Ville'][2].Population));
+            httpRequest.open((document.querySelector("#title").innerText = data.Title) +
+                (document.querySelector("#text").innerText = data.Desc) +
+                (document.querySelector("#météo").innerText = data.Météo) +
+                (document.querySelector("#population").innerText = data.Population));
         })
     httpRequest.open(document.querySelector("#image").src = "IMG/Moscou.jpg");
     httpRequest.send();
 }
 
-//Fonction qui charge un texte en ajax
+//Fonction qui execute des données sur Shangai.
 function CommentShangai() {
     httpRequest = new XMLHttpRequest();
 
+    // Si la requette ne fonctionne pas, une alerte pop-up s'affichera sur le site et le programme sort de la fonction.
     if (!httpRequest) {
         alert('Giving up :( Cannot create an XMLHTTP instance');
         return false;
     }
 
-    fetch("SCRIPT/data.json") // Lire le fichier JSON
-        //Parse le fichier JSON
+    //Cette fonction permet d'analyser et d'extraires des informations de Shangai
+    fetch("SCRIPT/Shangai.json") // Lire le fichier JSON
         .then(response => response.json())
         .then(data => {
-            httpRequest.open((document.querySelector("#title").innerText = data['Ville'][3].Title) +
-                (document.querySelector("#text").innerText = data['Ville'][3].Desc) +
-                (document.querySelector("#météo").innerText = data['Ville'][3].Météo) +
-                (document.querySelector("#population").innerText = data['Ville'][3].Population));
+            httpRequest.open((document.querySelector("#title").innerText = data.Title) +
+                (document.querySelector("#text").innerText = data.Desc) +
+                (document.querySelector("#météo").innerText = data.Météo) +
+                (document.querySelector("#population").innerText = data.Population));
         })
     httpRequest.open(document.querySelector("#image").src = "IMG/Shangai.jpg");
     httpRequest.send();
 }
 
-//Fonction qui charge un texte en ajax
+//Fonction qui execute des données sur Skopje.
 function CommentSkopje() {
     httpRequest = new XMLHttpRequest();
 
+    
+    // Si la requette ne fonctionne pas, une alerte pop-up s'affichera sur le site et le programme sort de la fonction.
     if (!httpRequest) {
         alert('Giving up :( Cannot create an XMLHTTP instance');
         return false;
     }
 
-    fetch("SCRIPT/data.json") // Lire le fichier JSON
-        //Parse le fichier JSON
+    // Si la requette ne fonctionne pas, une alerte pop-up s'affichera sur le site et le programme sort de la fonction.
+    fetch("SCRIPT/Skopje.json") // Lire le fichier JSON
         .then(response => response.json())
         .then(data => {
-            httpRequest.open((document.querySelector("#title").innerText = data['Ville'][4].Title) +
-                (document.querySelector("#text").innerText = data['Ville'][4].Desc) +
-                (document.querySelector("#météo").innerText = data['Ville'][4].Météo) +
-                (document.querySelector("#population").innerText = data['Ville'][4].Population));
+            httpRequest.open((document.querySelector("#title").innerText = data.Title) +
+                (document.querySelector("#text").innerText = data.Desc) +
+                (document.querySelector("#météo").innerText = data.Météo) +
+                (document.querySelector("#population").innerText = data.Population));
         })
     httpRequest.open(document.querySelector("#image").src = "IMG/Skopje.jpg");
     httpRequest.send();
